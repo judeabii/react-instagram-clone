fa0c7d18b0061ad0865458a280e62d27ab00f9b7
Jude Abishek Satish <jude-abishek-satish@hpe.com>
2024-03-26 02:35:30 +0530

    Build files added

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@chakra-ui/react': 'node_modules/@chakra-ui'
+    }
+  }
 })
